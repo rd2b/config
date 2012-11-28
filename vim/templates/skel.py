805e0eaa6383eecf09a85a86e060e57587c89ba8
Remi <rd@r-2b.net>
2012-11-28 15:59:53 +0100

    Ajout fonction pour headers

--- vim/templates/skel.py
+++ vim/templates/skel.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
-#########################################################
-#   Name:   .vim/templates/python       #
-#   Description: TODO   #
-#########################################################
 
+""" <file_name> : Description """
+
+__author__ = "<author>"
+__date__ = "<date>"
+
+
+import argparse
+
+
+def main():
+    """ Default main function """
+    # parse command line options
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--foo', help='foo help')
+    args = parser.parse_args()
+    print args.accumulate(args.integers)
+
+
+if __name__ == "__main__":
+    main()
