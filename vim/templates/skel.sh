53aa28a65fe167ae85e139481070121bafa467fd
Remi <rd@r-2b.net>
2013-01-18 07:54:30 +0100

    Ajout usage et options

--- vim/templates/skel.sh
+++ vim/templates/skel.sh
@@ -3,3 +3,43 @@
 #   Filename   : <file_name>                             #
 #   Description: TODO   #
 #########################################################
+
+PROGNAME="$(basename $0)"
+quiet=false
+
+set -u
+set -e
+
+# Prints help message
+function showhelp {
+    cat >&2 <<- EOF 
+Usage: $PROGNAME [OPTION] ...
+Options:
+    -h  prints this help message.
+EOF
+}
+
+function log {
+    local level="$1"
+    local message="$2"
+
+    $quiet || echo "$(date +"%F %T" ) $PROGNAME: $message" 
+}
+
+while getopts "o:qh" opt
+do
+    case $opt in
+        o)  option=$OPTARG
+            ;;
+        q)  quiet=true
+            ;;
+        h)  showhelp
+            exit 1
+            ;;
+        *)  showhelp
+            exit 1
+            ;;
+    esac
+done  
+
+echo $option
