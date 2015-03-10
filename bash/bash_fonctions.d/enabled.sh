ea83f7fcb26fba872d7fdac7e25815464a4e994e
Remi Debay <rdebay.ext@orange.com>
2015-03-10 12:42:46 +0100

    Deplacement des fonctions dans plusieurs fichiers

---
+++ bash/bash_fonctions.d/enabled.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#########################################################
+#   Filename   : enabled.sh                             #
+#   Description: TODO   #
+#########################################################
+
+cd ~/.myconfig/bash/bash_fonctions.d/
+
+. hosts.sh
+. handlesshagent.sh
+. updaterepository.sh
