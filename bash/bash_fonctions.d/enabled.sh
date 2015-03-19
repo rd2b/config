d81653a774ef39db8a13a290cf1b56f535b21f00
Remi Debay <rdebay.ext@orange.com>
2015-03-19 15:51:42 +0100

    Bug fix sur repertoire

--- bash/bash_fonctions.d/enabled.sh
+++ bash/bash_fonctions.d/enabled.sh
@@ -4,8 +4,11 @@
 #   Description: TODO   #
 #########################################################
 
+before=$(pwd)
 cd ~/.myconfig/bash/bash_fonctions.d/
 
 . hosts.sh
 . handlesshagent.sh
 . updaterepository.sh
+
+cd $before
